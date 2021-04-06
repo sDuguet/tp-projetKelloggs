ce26109a2ded6911ecf2430cca355fce2104b116
Simon <simdug33@gmail.com>
2021-04-06 03:04:59 +0200

    ajout du contact form

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -15,3 +15,23 @@ a2.addEventListener("click", (event) => {
   t1.classList.add("hide");
   t2.classList.remove("hide");
 });
+/*
+const inputs = document.querySelectorAll(".input");
+
+function addcl() {
+  let parent = this.parentNode.parentNode;
+  parent.classList.add("focus");
+}
+
+function remcl() {
+  let parent = this.parentNode.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", addcl);
+  input.addEventListener("blur", remcl);
+});
+*/
